e35e4754cee079976257d6f9e756c3ae942b169c
Bayardo Garcia <bayardogarciaqxfx@gmail.com>
2021-09-13 10:37:19 -0600

    Javascript Corregido y estilos corregidos

--- app.js
+++ app.js
@@ -7,26 +7,55 @@ class Product{
 }
 
 class UserInterface{
-    addProduct(){
+    addProduct(product){
+        const productList = document.getElementById("product-list");
+        const element = document.createElement("div");
+        /* Insertar elementos en el DOM mediante el innerHTML y las tildes(`) de JavaScript */
+        element.innerHTML = `
+        <div class="card mb-3">
+            <div class="card-body">
+                <strong>Product</strong>: ${product.name} -
+                <strong>Price</strong>: ${product.price} - 
+                <strong>Year</strong>: ${product.year}
+            </div>
+            <div class="card-body pt-0">
+                 <a href="#" class="btn btn-secondary" id="delete">Eliminar</a>
+            </div>
+        </div>
+        `;
+        productList.appendChild(element);
+    }
 
+    resetForm(){
+        document.getElementById("product-form").reset();
     }
     
-    deleteProduct(){
-
-    }
-
-    showMessage(){
-
+    deleteProduct(element){
+        if(element.id === "delete"){
+            element.parentElement.parentElement.parentElement.remove();
+        }
     }
 }
 
 //Eventos del DOM
-document.getElementById("product-form")
-    .addEventListener("submit", function(e){
-        e.preventDefault();
-        console.log(document.getElementById("name").value)
-})
+document.getElementById("product-form").addEventListener("submit", getProduct);
+document.getElementById("product-list").addEventListener("click", deleteProduct );
 
-/* document.getElementById("name").value;
-document.getElementById("price").value;
-document.getElementById("year").value; */
+function getProduct(e){
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const price = document.getElementById("price").value;
+    const year = document.getElementById("year").value; 
+
+    const product = (new Product(name, price, year));
+
+    const ui = new UserInterface();
+    ui.addProduct(product);
+    ui.resetForm();
+}
+
+
+function deleteProduct(e){
+    const ui = new UserInterface();
+    ui.deleteProduct(e.target);
+}
